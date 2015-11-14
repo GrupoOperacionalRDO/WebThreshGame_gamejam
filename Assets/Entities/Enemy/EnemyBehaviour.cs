ea53e68dc786a27cadbe5445a4e0db3a29a3da56
ArturN <arturnista@gmail.com>
2015-11-14 21:50:45 -0200

    Enemy sprites

--- Assets/Entities/Enemy/EnemyBehaviour.cs
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class EnemyBehaviour : MonoBehaviour {
 
+	public Sprite[] sprite;
+
+	private SpriteRenderer spriteRenderer;
+
 	void Start () {
 		GameObject parent = GameObject.Find ("Enemies");
 		if(!parent){
 			parent = new GameObject("Enemies");
 		}
 		transform.parent = parent.transform;
+
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.sprite = sprite[Random.Range(0, sprite.Length)];
 	}
 
 	public void DestroyEnemy(){
