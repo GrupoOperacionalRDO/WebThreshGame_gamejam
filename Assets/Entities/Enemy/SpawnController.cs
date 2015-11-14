77c1cf0773be0b270237827ca6dff2a6d3bd5bb2
Lorek <lorekqt@live.com>
2015-11-14 18:06:28 -0200

    Spawnable Enemies

---
+++ Assets/Entities/Enemy/SpawnController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+	
+	public GameObject enemyPrefab;
+
+	private float timer;
+	private float minX = -21.0f, minY = -5.0f, maxX = -19.0f, maxY = 4.0f, speed = 8;
+
+	// Use this for initialization
+	void Start () {
+		if (!enemyPrefab) {
+			Debug.LogError("No instance of enemy found");
+		}
+		InstantiateEnemy ();
+	}
+
+	void InstantiateEnemy(){
+
+		Vector3 position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY));
+		
+		GameObject enemy = Instantiate (enemyPrefab, position, Quaternion.identity) as GameObject;
+		
+		enemy.GetComponent<Rigidbody2D> ().velocity = enemy.transform.right * Random.Range (3,10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 3) {
+			InstantiateEnemy();
+
+			timer = 0;
+		}
+	}
+}
