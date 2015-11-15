c3806d1aff7652ed8101b54df96bdaf7b9fdb41a
Lorek <lorekqt@live.com>
2015-11-15 13:25:19 -0200

    Wall Bounce

--- Assets/Entities/Wall/Wall.cs
+++ Assets/Entities/Wall/Wall.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class Wall : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
-
+		GetComponent<BoxCollider2D> ().size = General.GetCameraSize ();
 	}
 
-	void OnTriggerEnter2D(Collider2D collision){
+	void OnTriggerExit2D(Collider2D collision){
 		EventManager.HandleMessage ("OnWallHit");
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 	
