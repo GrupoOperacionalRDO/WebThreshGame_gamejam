ea9e64c221e24862a15b1bff8df250292adde175
Lorek <lorekqt@live.com>
2015-11-15 01:06:53 -0200

    Speed upgrade and Score Fix

--- Assets/Entities/UIController/Score.cs
+++ Assets/Entities/UIController/Score.cs
@@ -4,13 +4,22 @@ using System.Collections;
 
 public class Score : MonoBehaviour {
 
-	public int score;
-	public Text scoreTxt;
+	private int score;
+	private Text scoreTxt;
 
+	public void SetScore(int scr){
+		score = scr;
+	}
+
+	public int GetScore(){
+		return score;
+	}
 	// Use this for initialization
 	void Start () {
 		EventManager.AddListener ("OnEnemyDestroyed", this.gameObject);
+
 		scoreTxt = GameObject.Find ("ScoreText").GetComponent<Text>();
+
 		if(!scoreTxt){
 			Debug.LogError("Can't find ScoreText");
 		}
--- Assets/Entities/UIController/UpgradeMenu/UpgradeController.cs
+++ Assets/Entities/UIController/UpgradeMenu/UpgradeController.cs
@@ -3,23 +3,26 @@ using System.Collections;
 
 public class UpgradeController : MonoBehaviour {
 	[SerializeField] private ThreshBehaviour thresh;
-	public Score scr;
+	public GameObject scrObject;
+	
 
 	// Use this for initialization
 	void Awake () {
 		thresh = GameObject.FindObjectOfType<ThreshBehaviour> ();
-		scr = GameObject.FindObjectOfType<Score> ();
+
 	}
 
 	public void OnSpeedButtonPress(){
+		Score scr = scrObject.GetComponent<Score> ();
+		int score = scr.GetScore ();
 
-		if (scr.score >= 2) {
+		if ((score - 2) >= 0) {
+			scr.SetScore (score - 2);
 			thresh.speed += 10;
-			scr.score -= 2;
+
 		}
 	}
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
