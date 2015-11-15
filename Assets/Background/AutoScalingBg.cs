8c3372bc9768b4865953dee511f018827986fd4a
João Teló <joaotelo.nh@hotmail.com>
2015-11-15 00:18:17 -0200

    background image (autoscaling)

---
+++ Assets/Background/AutoScalingBg.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScalingBg : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float quadHeight = Camera.main.orthographicSize * 2.0f;
+		float quadWidth = quadHeight * Screen.width / Screen.height;
+		this.GetComponent<Transform>().localScale = new Vector3(quadWidth, quadHeight, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
