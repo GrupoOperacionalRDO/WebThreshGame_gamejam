1d711f4cd156277db4b308bc90c3ce8dd7a4b617
Lorek <lorekqt@live.com>
2015-11-15 11:44:39 -0200

    Fixed Angle on HookThrow

--- Assets/Entities/Thresh/ThreshBehaviour.cs
+++ Assets/Entities/Thresh/ThreshBehaviour.cs
@@ -3,11 +3,9 @@ using System.Collections;
 
 public class ThreshBehaviour : MonoBehaviour {
 
-	public GameObject hookPrefab;
-
-	private GameObject hookHand;
-	private Vector3 positionThrow;
-	public float speed = 15.0f, maxRange = 3;
+	public GameObject hookPrefab, hookHand;
+	private Vector3 positionThrow, hookPos;
+	public float angle, speed = 15.0f, maxRange = 3;
 	public int value = 1;
 
 	void Start () {
@@ -23,31 +21,39 @@ public class ThreshBehaviour : MonoBehaviour {
 
 	void Update(){
 		if(Input.GetMouseButtonDown(0)){
+			positionThrow = Input.mousePosition;
+			CalculateAngle();
 			if(CanCreateHook()){
 				EventManager.HandleMessage("OnHookCreated");
-				positionThrow = Input.mousePosition;
 			}
 		}
 	}
 
 	bool CanCreateHook(){
+		print (angle);
 		if(GameObject.FindObjectsOfType<HookBehaviour>().Length > 0){
 			return false;
-		}
-
-		if (General.IsPointerOverUIObject()){
+		}else if (General.IsPointerOverUIObject()){
 			return false;
+		}else if (angle > 90 || angle < -90) {
+			return true;
 		}
-		return true;
+		return false;
+	}
+
+	void CalculateAngle(){
+
+		hookPos = hookHand.transform.position;
+
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		angle = Mathf.Atan2((mousePos.x - hookPos.x), (mousePos.y - hookPos.y)) * Mathf.Rad2Deg;
 	}
 
 	public void HookThrow() {
-		float angle;
-		Vector3 hookPos = hookHand.transform.position;
-		GameObject hookInstance = Instantiate (hookPrefab, hookPos, Quaternion.identity) as GameObject;
-		Vector2 mousePos = Camera.main.ScreenToWorldPoint(positionThrow);
-		angle = Mathf.Atan2((mousePos.x - hookPos.x), (mousePos.y - hookPos.y)) * Mathf.Rad2Deg;
-		hookInstance.transform.Rotate (0, 0, -angle);
-		hookInstance.GetComponent<HookBehaviour>().SetVelocity(speed);
+			GameObject hookInstance = Instantiate (hookPrefab, hookPos, Quaternion.identity) as GameObject;
+			hookInstance.transform.Rotate (0, 0, -angle);
+			hookInstance.GetComponent<HookBehaviour> ().SetVelocity (speed);
+
 	}
 }
